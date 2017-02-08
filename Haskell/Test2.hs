ac114b8ecd3b97c1574f29387e8f6ac995f4898e
Eden Cox <cox1100@student.nhl.nl>
2017-02-08 18:55:09 +0100

    Fixed code

--- Haskell/Test2.hs
+++ Haskell/Test2.hs
@@ -1,16 +1,18 @@
 import Debug.Trace
+import Control.Monad.ST
+import Control.Monad (foldM)
 
 shortestpath start end graph = enhance [(start,0)] end graph
 
 enhance collection end graph = if not (null enddistances)
-                                 then trace("The EndDistances: " ++ show enddistances) (head enddistances)
-                                 else trace("new Collection: " ++ show collection1) (enhance collection1 end graph)
-                                     where {enddistances = [a | (x,a) <- collection1, let x = end];
+                                 then head enddistances
+                                 else enhance collection1 end graph
+                                     where {enddistances = [a | (x,a) <- collection1, x == end];
                                            neighbours = [(y,a1+a2) | (x,a1) <- collection, 
                                                          (x1,list) <- graph, 
-                                                         let x1 = x, 
+                                                         x1 == x, 
                                                          (y,a2) <- list, 
-                                                         trace("Y: " ++ show y)(notElem y [a|(a,b) <- collection]) ]; 
-                                           collection1 = collection ++ [(x,a)|(x,a)<-neighbours, let a = trace("Minimun dist: " ++ show neighbours)(minimum [b| (x,b) <- neighbours])]}         
+                                                         notElem y [a|(a,b) <- collection] ]; 
+                                           collection1 = collection ++ [(x,a)|(x,a)<-neighbours, a == minimum [b| (x,b) <- neighbours]]}         
 
---shortestpath 'a' 'c' [('a', [('b', 3), ('c', 6)]), ('b', [('a', 3), ('c', 1)]), ('c', [('a', 6), ('b', 1)])]										   
+--shortestpath 'a' 'c' [('a', [('b', 3), ('c', 6)]), ('b', [('a', 3), ('c', 1)]), ('c', [('a', 6), ('b', 1)])]
