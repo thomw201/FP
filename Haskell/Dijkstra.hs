d2d66867574c56f3f580afdf27aace20ca30ea22
Eden Cox <cox1100@student.nhl.nl>
2017-02-08 23:24:47 +0100

    Changed dijkstra code

--- Haskell/Dijkstra.hs
+++ Haskell/Dijkstra.hs
@@ -1,56 +1,52 @@
 import Data.Array
 import Data.Array.MArray
 import Data.Array.ST
+import Text.Printf
+import Control.Exception
+import System.CPUTime
 import Control.Monad.ST
 import Control.Monad (foldM)
 import Data.Set as S
- 
-patternMatch :: String -> String
-patternMatch "Lol" = "Kek"
-patternMatch x = "Bleh"
-
---shortestpad start end graph path = findpath [(start,0)] end graph
---findpath visited '?' []
 
 dijkstra :: (Ix v, Num w, Ord w, Bounded w) => v -> v -> Array v [(v,w)] -> (Array v w, Array v v)
-dijkstra src invalid_index adj_list = runST $ do
+dijkstra startNode invalid_index adj_list = runST $ do
 
-  min_distance <- newSTArray b maxBound
-  writeArray min_distance src 0
-  previous <- newSTArray b invalid_index
-  let aux vertex_queue =
-        case S.minView vertex_queue of
+  minimumDistance <- newNodesArray nodes maxBound
+  writeArray minimumDistance startNode 0
+  prevList <- newNodesArray nodes invalid_index
+  let aux vertList =
+        case S.minView vertList of
           Nothing -> return ()
-          Just ((dist, u), vertex_queue') ->
-            let edges = adj_list ! u
-                f vertex_queue (v, weight) = do
-                  let dist_thru_u = dist + weight
-                  old_dist <- readArray min_distance v
-                  if dist_thru_u >= old_dist then
-                    return vertex_queue
+          Just ((dist, curNode), vertList') ->
+            let pathes = adj_list ! curNode
+                f vertList (nextNode, weight) = do
+                  let newDist = dist + weight
+                  old_dist <- readArray minimumDistance nextNode
+                  if newDist >= old_dist then
+                    return vertList
                   else do
-                    let vertex_queue' = S.delete (old_dist, v) vertex_queue
-                    writeArray min_distance v dist_thru_u
-                    writeArray previous v u
-                    return $ S.insert (dist_thru_u, v) vertex_queue'
+                    let vertList' = S.delete (old_dist, nextNode) vertList
+                    writeArray minimumDistance nextNode newDist
+                    writeArray prevList nextNode curNode
+                    return $ S.insert (newDist, nextNode) vertList'
             in
-            foldM f vertex_queue' edges >>= aux
-  aux (S.singleton (0, src))
-  m <- freeze min_distance
-  p <- freeze previous
+            foldM f vertList' pathes >>= aux
+  aux (S.singleton (0, startNode))
+  m <- freeze minimumDistance
+  p <- freeze prevList
   return (m, p)
-  where b = bounds adj_list
-        newSTArray :: Ix i => (i,i) -> e -> ST s (STArray s i e)
-        newSTArray = newArray
+  where nodes = bounds adj_list
+        newNodesArray :: Ix i => (i,i) -> e -> ST s (STArray s i e)
+        newNodesArray = newArray
 
-shortest_path_to :: (Ix v) => v -> v -> Array v v -> [v]
-shortest_path_to target invalid_index previous =
+getPath :: (Ix v) => v -> v -> Array v v -> [v]
+getPath target invalid_index prevList =
   aux target [] where
     aux vertex acc | vertex == invalid_index = acc
-                   | otherwise = aux (previous ! vertex) (vertex : acc)
+                   | otherwise = aux (prevList ! vertex) (vertex : acc)
 
-adj_list :: Array Char [(Char, Int)]
-adj_list = listArray ('a', 'n') [ [('b',2), ('e',2), ('h',2), ('k',2)], --A
+graph1 :: Array Char [(Char, Int)]
+graph1 = listArray ('a', 'n') [ [('b',2), ('e',2), ('h',2), ('k',2)], --A
                                   [('a',2), ('c',4), ('h',1), ('i',2)], --B
                                   [('b',4), ('d',3), ('i',2), ('j',4)], --C
                                   [('c',3), ('j',2), ('n',1)], --D
@@ -65,9 +61,31 @@ adj_list = listArray ('a', 'n') [ [('b',2), ('e',2), ('h',2), ('k',2)], --A
                                   [('f',3), ('g',1), ('j',2), ('l',3), ('n',3)], --M
                                   [('d',1), ('g',1), ('j',1), ('m',3)] ] --N
 
+graph2 :: Array Char [(Char, Int)]
+graph2 = listArray ('a', 'f') [[('b',2),('c',3)], --A
+                               [('a',2),('c',3),('d',2),('e',2)], --B
+                               [('a',2),('b',3),('e',2)], --C
+                               [('b',2),('e',1),('f',4)], --D	
+                               [('b',2),('c',2),('d',1),('f',3)], --E
+                               [('d',4),('e',3)]] --F							  
+
 main :: IO ()
 main = do
-  let (min_distance, previous) = dijkstra 'a' ' ' adj_list
-  putStrLn $ "Distance from a to n: " ++ show (min_distance ! 'n')
-  let path = shortest_path_to 'n' ' ' previous
+  start <- getCPUTime
+  let (minimumDistance, prevList) = dijkstra 'a' ' ' graph2
+  putStrLn $ "Distance from a to f: " ++ show (minimumDistance ! 'f')
+  let path = getPath 'f' ' ' prevList
   putStrLn $ "Path: " ++ show path
+  end   <- getCPUTime
+  let diff = (fromIntegral (end - start)) / (10^12)
+  printf "Computation time: %0.9f sec\n" (diff :: Double)
+  printf "Individual time: %0.9f sec\n" (diff / 10^6)
+  start <- getCPUTime
+  let (minimumDistance, prevList) = dijkstra 'a' ' ' graph1
+  putStrLn $ "Distance from a to n: " ++ show (minimumDistance ! 'n')
+  let path = getPath 'n' ' ' prevList
+  putStrLn $ "Path: " ++ show path
+  end   <- getCPUTime
+  let diff = (fromIntegral (end - start)) / (10^12)
+  printf "Computation time: %0.12f sec\n" (diff :: Double)
+  printf "Individual time: %0.12f sec\n" (diff / 10^6)
--- Haskell/Test2.hs
+++ Haskell/Test2.hs
@@ -1,11 +1,7 @@
-import Debug.Trace
-
 shortestpath start end graph = enhance [(start,0)] end graph
 
---getpad endcollection end = 
-
 enhance collection end graph = if not (null enddistances)
-                                 then trace(show collection1)(head enddistances)
+                                 then head enddistances
                                  else enhance collection1 end graph
                                      where {enddistances = [a | (x,a) <- collection1, x == end];
                                            neighbours = [(y,a1+a2) | (x,a1) <- collection, 
@@ -15,7 +11,7 @@ enhance collection end graph = if not (null enddistances)
                                                          notElem y [a|(a,b) <- collection] ]; 
                                            collection1 = collection ++ [(x,a)|(x,a)<-neighbours, a == minimum [b| (x,b) <- neighbours]]}         
 
-graph = [('a', [('b',2), ('e',2), ('h',2), ('k',2)]), 
+graph1 = [('a', [('b',2), ('e',2), ('h',2), ('k',2)]), 
          ('b', [('a',2), ('c',4), ('h',1), ('i',2)]), 
          ('c', [('b',4), ('d',3), ('i',2), ('j',4)]), 
          ('d', [('c',3), ('j',2), ('n',1)]), 
@@ -29,3 +25,11 @@ graph = [('a', [('b',2), ('e',2), ('h',2), ('k',2)]),
          ('l', [('f',1), ('i',1), ('j',2), ('k',4), ('m',3)]), 
          ('m', [('f',3), ('g',1), ('j',2), ('l',3), ('n',3)]), 
          ('n', [('d',1), ('g',1), ('j',1), ('m',3)])]
+
+--[('a',0),('b',2),('e',2),('h',2),('k',2),('i',4),('f',4),('l',5),('l',5),('c',6),('c',6),('m',7),('j',7),('j',7),('g',8),('g',8),('n',8),('n',8)]
+--a k f l j n
+
+-- :: IO ()
+--main = do
+  --let distance = getpath 'a' 'n' graph1
+  --putStrLn $ "Distance from a to n: " ++ show distance
